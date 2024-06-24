587a0966462961dca823cca28657b341ce8c38e9
Jam <jamhejratipro@gmail.com>
2024-06-24 18:20:45 +0330

    build: install daisyUI and add in tailwind's plugin config file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 export default function Home() {
   return (
-    <>Hello world!</>
+    <div className='flex w-screen h-screen bg-rose-200 items-center justify-center'>
+      <button className="btn">Button</button>
+    </div>
   );
 }
