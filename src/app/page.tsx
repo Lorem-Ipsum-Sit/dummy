19465d88d309e6180d03a6ad54effd883d8d626c
Jam <jamhejratipro@gmail.com>
2024-06-25 12:16:51 +0330

    build: add axios, eslint & prettier and set configs for lint and format

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import Button from '@/components/Button';
+import Cards from '@/components/Cards';
+
 export default function Home() {
   return (
-    <div className='flex w-screen h-screen bg-rose-200 items-center justify-center'>
+    <div className="flex w-screen h-screen bg-rose-200 items-center justify-center">
       <button className="btn">Button</button>
+      <Button />
+      <Cards />
     </div>
   );
 }
---
+++ src/components/Button/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Button() {
+  return <div className="bg-slate-100"></div>;
+}
+
+export default Button;
